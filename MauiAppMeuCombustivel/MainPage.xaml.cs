dd09312e1f50af9b05c1ee1ca8131e1c5ff1b9e6
henriquealbano <henriquealbanosn@gmail.com>
2025-03-17 22:36:24 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMeuCombustivel/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace MauiAppMeuCombustivel
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                double etanol = Convert.ToDouble(txt_etanol.Text);
+                double gasolina = Convert.ToDouble(txt_gasolina.Text);
+
+                string mensagem = "";
+
+                if (etanol >= (gasolina * 0.7))
+                {
+                    mensagem = "O Etanol está compensando!";
+                }
+                else
+                {
+                    mensagem = "A Gasolina está compensando!";
+                }
+
+                DisplayAlert("Calculado", mensagem, "Ok");
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "Fechar");
+            }
+        }
+    }
+
+}
